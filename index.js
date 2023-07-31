517e69fc29e1392d8eb9cebff2b92034d8561e60
Michelle Kyalo <muendikyalo@gmail.com>
2023-07-31 10:20:26 +0300

    Done with objects lab

--- index.js
+++ index.js
@@ -1,1 +1,29 @@
-// Write your solution in this file!
+// Object code
+const employee = {
+        name: "Sam",
+        streetAddress: "11 Broadway",
+};
+
+// returns clone with new data using spread operator
+function updateEmployeeWithKeyAndValue(employee, key, value){
+    return {...employee, [key]:value};
+};
+
+// Updates employee with the given key and value and returns the entire updated employee
+function destructivelyUpdateEmployeeWithKeyAndValue(employee, key,value){
+    employee[key] = value;
+    return employee;
+}
+// Delete key from a clone of the employee and returns new employee without modifying the new employee
+function deleteFromEmployeeByKey(employee, key){
+    const clonedEmployee = {...employee};
+    delete clonedEmployee[key];
+    return clonedEmployee;
+}
+clonedEmployee;
+ 
+//Return employee without the deleted key/ value pair, modifies the original employee.
+function destructivelyDeleteFromEmployeeByKey(employee, key){
+    delete employee[key];
+    return employee;
+}
